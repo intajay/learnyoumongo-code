0a4dc0d76dd2ba7c37fe91937ae852e6c1ed68e7
Ajay Meena <intajayk@gmail.com>
2017-01-18 01:14:25 +0530

    Initial commit

---
+++ find.js
@@ -0,0 +1,40 @@
+ var mongo = require('mongodb').MongoClient;
+
+ var url = 'mongodb://localhost:27017/data';
+
+ mongo.connect(url, function(err, db) {
+ 	var parrots = db.collection('parrots');
+
+ 	parrots.remove();
+
+ 	parrots.insert([{
+ 		species: "Macaws",
+ 		age: 75
+ 	}, {
+ 		species: "Cockatoos",
+ 		age: 40
+ 	}, {
+ 		species: "Amazons",
+ 		age: 55
+ 	}, {
+ 		species: "Eclectus",
+ 		age: 65
+ 	}, {
+ 		species: "Lovebirds",
+ 		age: 20
+ 	}]);
+
+ 	parrots.find({
+ 		age: {
+ 			$gt: parseInt(process.argv[2])
+ 		}
+ 	}, {
+ 		species: 1,
+ 		age: 1,
+ 		_id: 0
+ 	}).toArray(function(err, documents) {
+ 		console.log(documents);
+ 	});
+
+ 	db.close();
+ });
---
+++ insert.js
@@ -0,0 +1,19 @@
+ var mongo = require('mongodb').MongoClient;
+
+ var url = 'mongodb://localhost:27017/data';
+
+ mongo.connect(url, function(err, db) {
+ 	var user = db.collection('user');
+ 	user.remove();
+
+ 	var obj = {
+ 		firstName: process.argv[2],
+ 		lastName: process.argv[3]
+ 	};
+ 	
+ 	user.insert(obj, function(err, data) {
+ 		console.log(JSON.stringify(data));
+ 	});
+
+ 	db.close();
+ });
